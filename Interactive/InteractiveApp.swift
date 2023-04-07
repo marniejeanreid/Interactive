24301f79620f4cadb5c35dc78308fadc856b8b16
Marnie Reid <marnie@kodewithklossy.com>
2023-04-07 14:18:32 +0200

    Initial Commit

---
+++ Interactive/InteractiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveApp.swift
+//  Interactive
+//
+//  Created by scholar on 4/7/23.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
